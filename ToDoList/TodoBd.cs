80a68794985c91266278d69866ba435128d478b9
Theo <theo.gomes@outlook.com.br>
2024-10-22 15:54:00 -0300

    Primeiro Commit

---
+++ ToDoList/TodoBd.cs
@@ -0,0 +1,9 @@
+﻿namespace ToDoList;
+using Microsoft.EntityFrameworkCore;
+class TodoDb : DbContext
+{
+    public TodoDb(DbContextOptions<TodoDb> options)
+        : base(options) { }
+
+    public DbSet<Todo> Todos => Set<Todo>();
+}
